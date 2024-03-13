f4e495e62b29002e8cc7e9b583609e5e1b6d0290
wkopec5 <kopec.will@gmail.com>
2024-03-12 20:00:58 -0500

    Update
    Removed old example package

--- app/src/main/java/com/example/mvvmnewsappincompose/models/NewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mvvmnewsappincompose.models
-
-data class NewsResponse(
-    val articles: MutableList<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/example/mvvmnewsappincompose/models/Source.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mvvmnewsappincompose.models
-
-import com.google.gson.annotations.SerializedName
-
-data class Source(
-    val id: Any,
-    val name: String
-)
